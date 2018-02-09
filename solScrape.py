21f40a34ca47559231d34c920b03c36bc3511f8e
U-DESKTOP-GIPVFTC\ranga <rangaraj.t@gmail.com>
2018-02-09 03:22:07 -0500

    Got solscrape to scrape on all results of a Collocate search

--- solScrape.py
+++ solScrape.py
@@ -8,6 +8,7 @@ import rowData as rd
 from rowData import rowData
 import time
 import csv
+import os
 
 driver = webdriver.Chrome()
 driver.get("https://corpus.byu.edu/coca/")
@@ -15,12 +16,15 @@ time.sleep(2)
 
 numListInt = 0
 
-def putInCSV(listStuff):
-    with open('try.json', 'a') as outfile:
+def putInCSV(listStuff, name):
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    with open('data/'+name+'.json', 'a') as outfile:
         json.dump(listStuff, outfile)
 
 #search on first screen of copa
-def Search(phrase):
+def Search(phrase, key):
     print("starting search")
 
     #switch to frame inside page
@@ -32,6 +36,8 @@ def Search(phrase):
     #print(elem)
     tab_switch.click()
 
+    #begin looping here
+
     #populate word/phrase
     time.sleep(1)
     word = driver.find_element_by_xpath('//*[@id="p"]')
@@ -39,7 +45,7 @@ def Search(phrase):
 
     #populate collocates
     coll = driver.find_element_by_xpath('//*[@id="w2"]')
-    coll.send_keys("VERB")
+    coll.send_keys(key)
 
     #make five visible
     show = driver.find_element_by_xpath('//*[@id="collocatesSpanRow"]/td/table/tbody/tr/td[1]/a')
@@ -55,24 +61,10 @@ def Search(phrase):
     search = driver.find_element_by_xpath('//*[@id="submit1"]')
     search.click()
     print("\tpressed search")
+    driver.switch_to_default_content()
+    time.sleep(20)
 
-def itThroughWords():
-    print("clicking element 1")
-    #time.sleep(2)
-
-    #switch frames
-    frame = driver.find_element_by_name('x2')
-    driver.switch_to.frame(frame)
-
-    #numList = driver.find_element_by_xpath('/html/body/form/table[2]/tbody/tr[2]/td[4]/font')
-    #numListInt = int(numList.text)
-
-    sel = driver.find_element_by_xpath('/html/body/form/table[2]/tbody/tr[2]/td[3]/a')
-    sel.click()
-    print("\tfinished clicking element 1")
-    #return numListInt
-
-def getData():
+def getData(phrase, key, context):
     print("collecting data")
     #print(numListInt)
     frame = driver.find_element_by_name('x3')
@@ -81,17 +73,22 @@ def getData():
     run = True
     while(run):
         firstNum = driver.find_element_by_xpath('//*[@id="showCell_1_1"]/a')
-        firstNumInt = int(firstNum.text)
+        firstNumInt = (int(firstNum.text))
 
         listStuff = []
         count = 1
         fullCount = firstNumInt
-        while (fullCount<(firstNumInt+100)):
-            resNumber = driver.find_element_by_xpath('//*[@id="showCell_1_'+str(count)+'"]/a')
-            year = driver.find_element_by_xpath('//*[@id="showCell_2_'+str(count)+'"]/a')
-            medium = driver.find_element_by_xpath('//*[@id="showCell_3_'+str(count)+'"]/a')
-            publication = driver.find_element_by_xpath('//*[@id="showCell_4_'+str(count)+'"]/a')
-            sentence = driver.find_element_by_xpath('//*[@id="t1_'+str(count)+'"]')
+        while (count<100):
+            try:
+                resNumber = driver.find_element_by_xpath('//*[@id="showCell_1_'+str(count)+'"]/a')
+                year = driver.find_element_by_xpath('//*[@id="showCell_2_'+str(count)+'"]/a')
+                medium = driver.find_element_by_xpath('//*[@id="showCell_3_'+str(count)+'"]/a')
+                publication = driver.find_element_by_xpath('//*[@id="showCell_4_'+str(count)+'"]/a')
+                sentence = driver.find_element_by_xpath('//*[@id="t1_'+str(count)+'"]')
+            except:
+                count = 101
+                print("\tRan Out of elements to check")
+                break
 
             listStuff.append(rd.serialze(rowData(int(resNumber.text), int(year.text), medium.text, publication.text, sentence.text)))
             if(fullCount%10 == 0):
@@ -99,35 +96,56 @@ def getData():
             count = count + 1
             fullCount = fullCount + 1
 
-        putInCSV(listStuff)
+        name = phrase + '_' + key + '_' + context
+        putInCSV(listStuff, name)
         print("\twrote to txt finished")
 
-        nextButton = driver.find_element_by_xpath('//*[@id="resort"]/table/tbody/tr/td/a[7]')
+        #nextButton = driver.find_element_by_css_selector('//*[@id="resort"]/table/tbody/tr/td/a[6]') #//*[@id="resort"]/table/tbody/tr/td/text()[6] //*[@id="resort"]/table/tbody/tr/td/a[7]
+
+        nextButton = driver.find_element_by_xpath('//*[@id="resort"]/table/tbody/tr/td/a[6]')
+        print("|"+nextButton.text+"|")
+        if not (nextButton.text=='>  '):
+            print("Switched > element")
+            nextButton = driver.find_element_by_xpath('//*[@id="resort"]/table/tbody/tr/td/a[7]')
+
         nextButton.click()
+
         time.sleep(5)
+
         nextNum = int(driver.find_element_by_xpath('//*[@id="showCell_1_1"]/a').text)
         if(nextNum == firstNumInt):
+            print("ran out of elements in " + context + " to look at.")
             run = False
 
+    driver.switch_to.default_content()
 
+    driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frameset/frame'))
+    freq = driver.find_element_by_xpath('//*[@id="label2"]')
+    freq.click()
+    print("clicked back to frequencies")
+    time.sleep(1)
+    driver.switch_to_default_content()
 
+def itThroughWords():
+    itCount = 2   #to test: change value to 101 and change while to: itCount>=100 || Should be 2 otherwise
+    while(itCount<=101):
+        num = itCount-1
+        print("clicking element " + str(num))
+        #switch frames
+        frame = driver.find_element_by_name('x2')
+        driver.switch_to.frame(frame)
 
+        sel = driver.find_element_by_xpath('/html/body/form/table[2]/tbody/tr['+str(itCount)+']/td[3]/a')
+        context = sel.text
+        sel.click()
+        driver.switch_to_default_content()
+        time.sleep(10)
+        print("\tfinished clicking element " + str(num))
+        getData(phrase, key, context)
+        itCount += 1
 
 phrase = "who"
-Search(phrase)
-driver.switch_to_default_content()
-
-time.sleep(20)
-#numlistInt =
+key = "VERB"
+Search(phrase, key)
 itThroughWords()
-driver.switch_to_default_content()
-
-time.sleep(10)
-getData()
-
-
-#driver.close()
-{
-    varname:varvue
-
-}
+driver.close()
