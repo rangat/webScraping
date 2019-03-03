a1a51d0b83443a13fb236b5531428ce54666ff58
Rangaraj Tirumala <rangaraj.t@gmail.com>
2019-03-03 00:29:05 -0500

    run takes user input now

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 import solScrape
 
-phrase = "how"
+p = input("What phrase do you want to search for? ")
+w = input("What word do you want to search for (know)? ")
+start = input("What number do you want to start at? ")
+
+if start == "":
+    start = None
+print(start)
+
+phrase = p
 key = "VERB"
 solScrape.search(phrase, key)
-solScrape.findWord(phrase, key, "know", start_at=43899)
+solScrape.findWord(phrase, key, w, start_at=start)
 solScrape.closeDriver()
